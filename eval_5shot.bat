bc20ebd48ee674677d85dd8c54a9929f25437024
Rakib <r.rakib.1074@gmail.com>
2025-07-23 02:50:43 +0600

    2025-07-23: Update all

--- eval_5shot.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d H:\University\4-1\RME-4100\MY_CODE\BPA
-python train.py --eval True --test_episodes 100 --pretrained_path "./checkpoints/miniimagenet/wrn/pt_map_bpa/-n_way=5-n_shot=3-lr=5e-05-scheduler=step-dropout=0.0/max_acc.pth"
-pause
--- train_model.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d H:\University\4-1\RME-4100\MY_CODE\BPA
-python train.py --train_episodes 1 --max_epochs 1 --num_shot 5 --num_query 1 --augment True
-pause
